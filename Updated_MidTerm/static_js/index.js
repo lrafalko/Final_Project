adadde9df2ce04b56f6333119cf7bae74319cd5b
Lance <lancealan.2021@gmail.com>
2022-11-12 12:34:57 -0500

    Added seasonal options and an animation for the home page using Javascript and css

---
+++ Updated_MidTerm/static_js/index.js
@@ -0,0 +1,54 @@
+const scroll_ele = document.querySelectorAll(".scroll_ele");
+
+console.log(scroll_ele);
+
+scroll_ele.forEach((element) => {
+    element.style.opacity = 0; 
+});
+
+
+
+const eleInView = (el, percentScroll = 100) => {
+    topEle = el.getBoundingClientRect().top;
+
+    return (
+        topEle <= ((window.innerHeight || document.documentElement.clientHeight) * (percentScroll / 100))
+    )
+
+}
+
+const displayScrolled = (ele) => {
+    ele.classList.add("scrolled");
+    ele.style.opacity = 1;
+}
+
+const hideScrolled = (ele) => {
+    ele.classList.remove("scrolled");
+    ele.style.opacity = 0;
+}
+
+const checkAnimation = () => {
+    scroll_ele.forEach((ele) => {
+        if(eleInView(ele, 100)) {
+            displayScrolled(ele); 
+        }
+
+        else {
+            hideScrolled(ele); 
+        }
+
+    })
+    
+}
+
+window.addEventListener("scroll", () => {
+    checkAnimation();
+}); 
+
+
+
+
+for(let i = 0; i < scroll_ele.length; i++){
+    scroll_ele[i].after()
+
+}
